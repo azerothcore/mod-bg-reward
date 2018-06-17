37e553d00cbbc58d80b5d3c06688764996f119c2
Kargatum <dowlandtop@yandex.ru>
2018-06-18 03:01:52 +0700

    Init repo

--- README.md
+++ README.md
@@ -3,29 +3,37 @@
 ### This is a module for [AzerothCore](http://www.azerothcore.org)
 
 #### Features:
-- This module notifies the player (who wrote a private message) that his interlocutor can not talk (if his chat is not available)
+- If you win or lose on the battleground, you get an additional reward
 
 #### Config option (BGReward.conf.dist)
-- BGReward.Enable (Enable/Disable module)
+- BG.Reward.Enable - Enable this module.
+- BG.Reward.ItemID.WSG - Reward item on battlegroud Warsong Gulch.
+- BG.Reward.ItemID.Arathi - Reward item on battlegroud Arathi Basin.
+- BG.Reward.ItemID.Alterac - Reward item on battlegroud Alterac Valley.
+- BG.Reward.ItemID.Isle - Reward item on battlegroud Isle of Conquest.
+- BG.Reward.ItemID.Ancients - Reward item on battlegroud Strand of the Ancients.
+- BG.Reward.ItemID.Eye - Reward item on battlegroud Eye of the Storm.
+- BG.Reward.WinnerTeam.Count - Reward count winner battlegroud team.
+- BG.Reward.LoserTeam.Count - Reward count loser battlegroud team.
 
 ### BGReward Module currently requires:
-- AzerothCore v1.0.1+
+- Warning! At the moment this module does not work on the official repository until will not be accepted PR (https://github.com/azerothcore/azerothcore-wotlk/pull/929)
+- For work need new hooks (https://github.com/Winfidonarleyan/azerothcore-wotlk/commit/049386b93662ad62ea6f779ed5832865ffbde242)
+- You can use this repository for testing (https://github.com/Winfidonarleyan/azerothcore-wotlk/tree/bg_hooks)
 
 ### How to install
 1. Simply place the module under the `modules` folder of your AzerothCore source folder.
 2. Re-run cmake and launch a clean build of AzerothCore
-3. Apply sql file in DB world `(./sql/world.trinity_string.sql)`
-4. (Optional) Edit module configuration (BGReward.conf.dist)
-5. Done :)
+3. (Optional) Edit module configuration (BGReward.conf.dist)
+4. Done :)
 
 ### Usage
-- Write to the player, whose chat is disabled
+- Win or lose BG
+- Look your currency
 
 ## Credits
-@Winfidonarleyan (author of the module)
-
-@BarbzYHOOL: Best guy
-
-@Talamortis: Almost best guy
+* [Winfidonarleyan](https://github.com/Winfidonarleyan) (author of the module): 
+* [BarbzYHOOL](https://github.com/barbzyhool): best guy
+* [Talamortis](https://github.com/talamortis): almost best guy
 
 AzerothCore: [repository](https://github.com/azerothcore) - [website](http://azerothcore.org/) - [discord chat community](https://discord.gg/PaqQRkd)
---
+++ src/BGReward.cpp
@@ -0,0 +1,98 @@
+#include "ScriptMgr.h"
+#include "Configuration/Config.h"
+
+class Kargatum_Reward_Battlegroud : public BGScript
+{
+public:
+    Kargatum_Reward_Battlegroud() : BGScript("Kargatum_Reward_Battlegroud") {}
+
+    void OnBattlegroudEndReward(Battleground* bg, Player* player, TeamId winnerTeamId) override
+    {
+        if (!sConfigMgr->GetBoolDefault("BG.Reward.Enable", true))
+            return;
+
+        // Reward ItemID
+        uint32 ItemID_WSG       = sConfigMgr->GetIntDefault("BG.Reward.ItemID.WSG", 20558);
+        uint32 ItemID_ARATHI    = sConfigMgr->GetIntDefault("BG.Reward.ItemID.Arathi", 20559);
+        uint32 ItemID_ALTERAC   = sConfigMgr->GetIntDefault("BG.Reward.ItemID.Alterac", 20560);
+        uint32 ItemID_ISLE      = sConfigMgr->GetIntDefault("BG.Reward.ItemID.Isle", 47395);
+        uint32 ItemID_ANCIENT   = sConfigMgr->GetIntDefault("BG.Reward.ItemID.Ancients", 42425);
+        uint32 ItemID_EYE       = sConfigMgr->GetIntDefault("BG.Reward.ItemID.Eye", 29024);
+
+        // Reward Count
+        uint32 WinnerCount      = sConfigMgr->GetIntDefault("BG.Reward.WinnerTeam.Count", 3);
+        uint32 LoserCount       = sConfigMgr->GetIntDefault("BG.Reward.LoserTeam.Count", 1);
+
+        TeamId bgTeamId = player->GetBgTeamId();
+        uint32 RewardCount = 0;
+
+        bgTeamId == winnerTeamId ? (RewardCount = WinnerCount) : (RewardCount = LoserCount);
+
+        switch (player->GetZoneId())
+        {
+        case 3277: // Warsong Gulch
+            player->AddItem(ItemID_WSG, RewardCount);
+            break;
+        case 3358: // Arathi Basin
+            player->AddItem(ItemID_ARATHI, RewardCount);
+            break;
+        case 3820: // Eye of the Storm
+            player->AddItem(ItemID_EYE, RewardCount);
+            break;
+        case 4710: // Isle of Conquest
+            player->AddItem(ItemID_ISLE, RewardCount);
+            break;
+        case 4384: // Strand of the Ancients
+            player->AddItem(ItemID_ANCIENT, RewardCount);
+            break;
+        case 2597: // Alterac Valley
+            player->AddItem(ItemID_ALTERAC, RewardCount);
+            break;
+        default:
+            break;
+        }
+    }
+};
+
+class Kargatum_Reward_Battlegroud_World : public WorldScript
+{
+public:
+    Kargatum_Reward_Battlegroud_World() : WorldScript("Kargatum_Reward_Battlegroud_World") {}
+
+    void OnAfterConfigLoad(bool /*reload*/) override
+    {
+        std::string conf_path = _CONF_DIR;
+        std::string cfg_file = conf_path + "/BGReward.conf";
+
+#if PLATFORM == PLATFORM_WINDOWS
+        cfg_file = "BGReward.conf";
+#endif       
+        std::string cfg_def_file = cfg_file + ".dist";
+
+        // Load .conf.dist config
+        if (!sConfigMgr->LoadMore(cfg_def_file.c_str()))
+        {
+            sLog->outString();
+            sLog->outError("Module config: Invalid or missing configuration dist file : %s", cfg_def_file.c_str());
+            sLog->outError("Module config: Verify that the file exists and has \'[worldserver]' written in the top of the file!");
+            sLog->outError("Module config: Use default settings!");
+            sLog->outString();
+        }
+
+        // Load .conf config
+        if (!sConfigMgr->LoadMore(cfg_file.c_str()))
+        {
+            sLog->outString();
+            sLog->outError("Module config: Invalid or missing configuration file : %s", cfg_file.c_str());
+            sLog->outError("Module config: Verify that the file exists and has \'[worldserver]' written in the top of the file!");
+            sLog->outError("Module config: Use default settings!");
+            sLog->outString();
+        }
+    }
+};
+
+void AddBGRewardScripts()
+{
+	new Kargatum_Reward_Battlegroud();
+    new Kargatum_Reward_Battlegroud_World();
+}
