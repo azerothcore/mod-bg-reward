a9a06cb18ffa47f1e851553cc30ecf2f0d1491eb
Kargatum <dowlandtop@yandex.ru>
2018-06-24 01:04:37 +0700

    Current cur dir

--- src/cmake/after_ws_install.cmake
+++ src/cmake/after_ws_install.cmake
@@ -2,14 +2,14 @@ if(WIN32)
   if (MSVC)
     add_custom_command(TARGET worldserver
       POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy ${CUR_DIR_MODULE}/conf/BGReward.conf.dist ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
+      COMMAND ${CMAKE_COMMAND} -E copy ${CUR_DIR_MODULE_BG_REWARD}/conf/BGReward.conf.dist ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
     )
   elseif (MINGW)
     add_custom_command(TARGET worldserver
       POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy ${CUR_DIR_MODULE}/conf/BGReward.conf.dist ${CMAKE_BINARY_DIR}/bin/
+      COMMAND ${CMAKE_COMMAND} -E copy ${CUR_DIR_MODULE_BG_REWARD}/conf/BGReward.conf.dist ${CMAKE_BINARY_DIR}/bin/
     )
   endif()
 endif()
 
-install(FILES "${CUR_DIR_MODULE}/conf/BGReward.conf.dist" DESTINATION ${CONF_DIR})
+install(FILES "${CUR_DIR_MODULE_BG_REWARD}/conf/BGReward.conf.dist" DESTINATION ${CONF_DIR})
